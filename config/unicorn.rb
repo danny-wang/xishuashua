394826d900da11a10fc0b2878360b5ca413d4cdd
danny_wang <danny.wang@aliyun.com>
2015-12-08 07:30:31 +0000

    modify a little

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-worker_processesInteger(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
 before_fork do |server,worker|
 Signal.trap 'TERM' do
-  puts'Unicorn master intercepting TERM and sending myself QUIT instead'
+  puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
   Process.kill 'QUIT', Process.pid
 end
 defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!

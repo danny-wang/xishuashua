203e8598a93a61991967f5943623cf59fdda5f92
danny_wang <danny.wang@aliyun.com>
2016-01-01 08:26:39 +0000

    finish cart-feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def hello
     render text:"hello world"
   end
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+     cart=Cart.create
+     session[:cart_id]=cart.id
+    cart
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,18 @@ class Product < ActiveRecord::Base
   validates :description, :image_url, :title, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  
+  #default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_itme
 
+ private
+  def ensure_not_referenced_by_any_line_itme
+   if line_items.empty?
+    return true
+   else
+    errors.add(:base,'Line Items present')
+    return false
+   end
+  end
 end

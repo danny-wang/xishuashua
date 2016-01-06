de1ba43f2a67be8271968715bfda8f0a0fd35bac
danny_wang <danny.wang@aliyun.com>
2016-01-06 16:36:10 +0000

    modify

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
+    
     @cart = current_cart
     if @cart.line_items.empty?
       redirect_to root_url, :notice => "Your cart is empty"
